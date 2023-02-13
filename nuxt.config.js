b86f71ec90a88e06934faf986cd363e440a4389f
belouso4K <belousovk495@gmail.com>
2023-02-13 16:26:37 +0300

    made changes for old version of nodejs

--- nuxt.config.js
+++ nuxt.config.js
@@ -163,6 +163,9 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    transpile: [
+      'defu',
+    ],
     splitChunks: {
       layouts: true
     }
